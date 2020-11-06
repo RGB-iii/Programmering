de61661354165ec2b4ab4e2c90488bb14e53ecab
RGB-iii <73332983+RGB-iii@users.noreply.github.com>
2020-11-06 09:44:56 +0100

    Level 3 - Smiley Stamps

--- functions - DrawHappyFace/sketch.js
+++ functions - DrawHappyFace/sketch.js
@@ -1,67 +1,68 @@
 var x, y;
 var radius;
-var blink;
 
 function setup() {
   createCanvas(600, 400);
   ellipseMode(CENTER);
   noStroke();
-  
-  x = 300; 
+  background(230);
+
+  x = 300;
   y = 200;
   radius = 100;
-  blink = false;
+  
 }
 
 function draw() {
-    background(230);
-    if(mouseIsOver()){
-      blink = true;
-    }
-    else{
-      blink = false;
-    }
-  
-    if(key == 's'){
-      sick();
-    }
-    else{
-      happy();
-    }
-  
-    
+
 }
 
-function happy(){
+function mouseClicked(){
+  //pick a random number between 0 and 1
+  var r = random();
+  //if the number is bigger than 0.5 (which is likely to happen half of the time)
+  if(r > 0.5){
+    drawHappyFace(mouseX, mouseY, random());
+  }
+  else{
+    drawSickFace(mouseX, mouseY, random());
+  }
+  
+
+}
+
+function keyPressed(){
+  if(key =='C'){
+    background(230);
+  }
+}
+
+function drawHappyFace(x, y, s){
    push();
-   translate(x, y);
-   
+   translate(x,y);
+   scale(s);
    //Face
    fill(249,205,173);//rosy beige
-   ellipse(0, 0, 100, 100);
-   
+   ellipse(0, 0, radius, radius);
+ 
    //Eye 1
    fill(30);//dark gray
    ellipse(0, 10, 10, 10);
+ 
+   //Eye 2
+   ellipse(20, 10, 10, 10);
    
-   if(blink){
-     rect(10, 10, 10, 2);
-   }
-   else{
-     ellipse(20, 10, 10, 10);
-   }
-   
+ 
    //Mouth
    fill(252,157,154);//light pink
    arc(0, 25, 30, 30, 0, radians(180), PIE); 
-   
    pop();
  }
  
- function sick(){
+ function drawSickFace(x, y, s){
    push();
    translate(x, y);
-   
+   scale(s);
    //Face
    fill(233,247,172);//greenish
    ellipse(0, 0, 100, 100);
@@ -81,16 +82,3 @@ function happy(){
    
    pop();
  }
-
- function mouseIsOver(){
-   var result;
-   var d = dist(mouseX, mouseY, x, y);
-   if (d < radius) {
-     result = true;
-   } else {
-     result = false;
-   }
-   return result;
- }
- 
- 
