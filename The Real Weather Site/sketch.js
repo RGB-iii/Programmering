8c5a0f800621174c49146ba385e6ea24613693b2
RGB-iii <73332983+RGB-iii@users.noreply.github.com>
2021-01-15 15:01:15 +0100

    Den endelige side (færdig)

--- The Real Weather Site/sketch.js
+++ The Real Weather Site/sketch.js
@@ -1,25 +1,42 @@
-importScripts("index.html");
-
 var icon;
 var weather;
 var temp;
 
+var api = 'http://api.openweathermap.org/data/2.5/weather?';
+var apiKey = '&APPID=a3e9549bfd2396cc888a0e2b5e780dd4';
+var units = '&units=metric';
+var city = 'q=Copenhagen';
+
+var input;
+
 function setup() {
 
-    loadJSON('http://api.openweathermap.org/data/2.5/weather?q=Copenhagen&units=metric&APPID=a3e9549bfd2396cc888a0e2b5e780dd4' , 
-    gotData
-    )
+    var button = select('#submit');
+  button.mousePressed(weatherAsk);
+
+    
 }
+
+function weatherAsk() {
+    input = select('#city');
+    city = 'q=' + input.value();
+
+    var link = api + city + units + apiKey
+
+
+    loadJSON(link , gotData)
+  }
 
 
 function gotData(data) {
+    cityName = data.name;
     icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"; 
     weather = data.weather[0].description;
     temp = Math.floor(data.main.temp);
-    city = data.name;
+    
 
+    createP(cityName);
     createImg(icon);
     createP(weather);
     createP(temp);
-    createP(city);
 }
