b94ff195f0f9a74dedd0ca2be4abc0348045c532
RGB-iii <73332983+RGB-iii@users.noreply.github.com>
2021-01-17 17:42:44 +0100

    Færdige side (med kommentarer)

--- The Real Weather Site/sketch.js
+++ The Real Weather Site/sketch.js
@@ -1,7 +1,10 @@
+//Jeg gør de fire elementer jeg gerne vil vise på min side til variabler.
 var icon;
 var weather;
 var temp;
 
+//Her henter jeg mit api, jeg splitter linket op i flere dele/variabler, 
+//så jeg nemt kan tilgå forskellige elementer af linket.
 var api = 'http://api.openweathermap.org/data/2.5/weather?';
 var apiKey = '&APPID=a3e9549bfd2396cc888a0e2b5e780dd4';
 var units = '&units=metric';
@@ -9,12 +12,14 @@ var city = 'q=Copenhagen';
 
 var input;
 
+//Jeg laver en funktion, der connecter søgefeltet til søgeknappen. 
+//Det gør jeg ved at tilføje en weatherAsk funktion.
+//Koden nedenunder "connecter" også dette søgefelt til en variabel jeg har kaldt for "city".
+//Denne variabel finder data/navnet på den bestemte by, man søger på fra det valgte API.
 function setup() {
 
     var button = select('#submit');
   button.mousePressed(weatherAsk);
-
-    
 }
 
 function weatherAsk() {
@@ -23,11 +28,11 @@ function weatherAsk() {
 
     var link = api + city + units + apiKey
 
-
     loadJSON(link , gotData)
   }
 
-
+//Nu "caller" jeg den data jeg har brug for fra mit API og skriver hvor programmet kan finde dem. 
+//f.eks. data.name er dér i API'et hvor man kan finde navnet på den valgte by.
 function gotData(data) {
     cityName = data.name;
     icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"; 
