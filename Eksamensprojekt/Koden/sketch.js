4ee1bb2bbefe4ad3174093c8c01848ca0be8806a
RGB-iii <73332983+RGB-iii@users.noreply.github.com>
2021-03-26 09:31:27 +0100

    createCapture virker + en generelt pænere kode

--- Eksamensprojekt/Koden/sketch.js
+++ Eksamensprojekt/Koden/sketch.js
@@ -28,57 +28,58 @@ let YesImage;
 
 // Load Image Classifier model
 function preload() {
-    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 
-    // Load Sound Classifier model
-function preload() {
-    classifier = ml5.soundClassifier(modelURL + 'model.json');
-  }
+  // Load Sound Classifier model
+  classifier = ml5.soundClassifier(modelURL + 'model.json');
+}
 
+//Dette kan ændres (Image Classifier)
+QuestionImage = loadImage('Question.png');
+IdeaImage = loadImage('Idea.png');
+IdkImage = loadImage('Idk.png');
+LoveImage = loadImage('Love.png');
+NoImage = loadImage('No.png');
+TiredImage = loadImage('Tired.png');
+YesImage = loadImage('Yes.png');
 
-    //Dette kan ændres (Image Classifier)
-    QuestionImage = loadImage('Question.png');
-    IdeaImage = loadImage('Idea.png');
-    IdkImage = loadImage('Idk.png');
-    LoveImage = loadImage('Love.png');
-    NoImage = loadImage('No.png');
-    TiredImage = loadImage('Tired.png');
-    YesImage = loadImage('Yes.png');
-  }
 
 function setup() {
-    createCanvas(640, 520);
+  createCanvas(640, 520);
 
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video)
+  // Start classifying
+  classifyVideo();
 
   // classify audioen (lytter til mikrofonen by default)
   classifyAudio();
+
+
 }
+
 function classifyAudio() {
-    classifier.classify(gotResults);
-  }
+  classifier.classify(gotResults);
+}
 
-    // Create the video
-    video = createCapture(VIDEO);
-    video.size(width, height);
-    video.hide();
-  
-    flippedVideo = ml5.flipImage(video)
-    // Start classifying
-    classifyVideo();
-  
-  function draw() {
 
-    textAlign(CENTER, CENTER);
+function draw() {
 
-    // Når der er baggrundslyd vises hovedtelefonerne
-    //Dette kan ændres (Sound Classification)
+  textAlign(CENTER, CENTER);
+
+  // Når der er baggrundslyd vises hovedtelefonerne
+  //Dette kan ændres (Sound Classification)
   let emoji = "🎧";
 
-  if (label == "Ko") {
+  if (label1 == "Ko") {
     emoji = "🐮";
-  } else if (label == "Kat") {
+  } else if (label1 == "Kat") {
     emoji = "🐱";
-  } else if (label == "Gris") {
+  } else if (label1 == "Gris") {
     emoji = "🐷";
   }
   // Opret/tegn emojisne
@@ -86,8 +87,8 @@ function classifyAudio() {
   text(emoji, width / 2, height / 2);
 
 
-// Classification
-function gotResults(error, results) {
+  // Classification
+  function gotResults(error, results) {
     if (error) {
       console.error(error);
       return;
@@ -96,64 +97,64 @@ function gotResults(error, results) {
     label = results[0].label;
   }
 
-    // Opret videoen
-    image(flippedVideo, 0, 0);
-    
-    //Dette kan ændres (Image Classification)
-    if (label == "Question" && confidence > 0.95){
-      image(QuestionImage, 50, 50);
-    }
-    
-     if (label == "Idea" && confidence > 0.90){
-      image(IdeaImage, 50, 5, 200, 200);
-    }
-    
-     if (label == "Idk" && confidence > 0.60){
-      image(IdkImage, 50, 50);
-    }
-    
-     if (label == "Love" && confidence > 0.001){
-      image(LoveImage, 50, 50, 200, 200);
-    }
-    
-     if (label == "No" && confidence > 0.10){
-      image(NoImage, 50, 50, 200, 200);
-    }
-    
-     if (label == "Tired" && confidence > 0.90){
-      image(TiredImage, 50, 50, 200, 200);
-    }
-    
-     if (label == "Yes" && confidence > 0.50){
-      image(YesImage, 50, 50, 200, 200);
-    }
-  
-    // Opret/tegn "label"
-    fill(255);
-    textSize(16);
-    textAlign(CENTER);
-    text(label, width / 2, height - 4);
-    text(confidence, width / 2, height - 20);
-  }
-  
-  // Get a prediction for the current video frame
-  function classifyVideo() {
-    flippedVideo = ml5.flipImage(video)
-    classifier.classify(flippedVideo, gotResult);
-  }
-  
-  // Når vi får et resultat
-  function gotResult(error, results) {
-    // Hvis der er en fejl
-    if (error) {
-      console.error(error);
-      return;
-    }
+  // Opret videoen
+  image(flippedVideo, 0, 0);
 
-    // Henter resultat fra et erray
-    label = results[0].label;
-    confidence = results[0].confidence;
-    
-    // Classify en sidste gang
-    classifyVideo();
+  //Dette kan ændres (Image Classification)
+  if (label == "Question" && confidence > 0.95) {
+    image(QuestionImage, 50, 50);
   }
+
+  if (label == "Idea" && confidence > 0.90) {
+    image(IdeaImage, 50, 5, 200, 200);
+  }
+
+  if (label == "Idk" && confidence > 0.60) {
+    image(IdkImage, 50, 50);
+  }
+
+  if (label == "Love" && confidence > 0.001) {
+    image(LoveImage, 50, 50, 200, 200);
+  }
+
+  if (label == "No" && confidence > 0.10) {
+    image(NoImage, 50, 50, 200, 200);
+  }
+
+  if (label == "Tired" && confidence > 0.90) {
+    image(TiredImage, 50, 50, 200, 200);
+  }
+
+  if (label == "Yes" && confidence > 0.50) {
+    image(YesImage, 50, 50, 200, 200);
+  }
+
+  // Opret/tegn "label"
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+  text(confidence, width / 2, height - 20);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// Når vi får et resultat
+function gotResult(error, results) {
+  // Hvis der er en fejl
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Henter resultat fra et erray
+  label = results[0].label;
+  confidence = results[0].confidence;
+
+  // Classify en sidste gang
+  classifyVideo();
+}
